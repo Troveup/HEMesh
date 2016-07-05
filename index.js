607080c3c48cf50db3daaf0c5fda5be27eeea8ab
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2016-07-05 15:20:03 -0400

    add objexporter functionality, move test code into HELib

--- index.js
+++ index.js
@@ -1,99 +1,206 @@
 
 var THREE = require("three");
 var HEMesh = require("./src/mesh");
-//var HEDebug = require("./src/debug.js");
+
+console.log("getting latest hemesh index.js");
 
 var OrbitControls = require('three-orbit-controls')(THREE);
 THREE.OrbitControls = OrbitControls;
 
 var CSGLib = require('./src/csg');
 
-function test() {
+var material = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    color: 0x00ff00
+});
+var iterativeMat = new THREE.MeshBasicMaterial({
+    color: 0x0000ff
+});
+var debugMat = new THREE.MeshNormalMaterial();
+
+var interiorColor = new THREE.Color( 0xff0000 );
+var frontierColor = new THREE.Color( 0x00ff00 );
+var boundaryColor = new THREE.Color( 0x0000ff );
+
+
+function exportToObj( scene ) {
+    var exporter = new THREE.OBJExporter();
+    return exporter.parse( scene );
 }
 
-module.exports = { HEMesh, CSGLib };
+function testCSG( scene, container ) {
+    var geometry1 = new THREE.SphereGeometry( 2, 16, 16 );
+    var geometry2 = new THREE.SphereGeometry( 2, 16, 16 );
 
-/*module.exports = (function(){
-    var that = {};
+    // var geometry1 = new THREE.BoxGeometry( 2, 2, 2 );
+    // var geometry2 = new THREE.BoxGeometry( 2, 2, 2 );
 
-    var fromThreeMesh = function(threeMesh) {
-        var geo = threeMesh.geometry;
-        var mesh = heMesh.create();
-        mesh.parseGeometry(geo);
-        mesh.fillHoles();
-        return mesh;
+    var mesh1 = new THREE.Mesh( geometry1, debugMat );
+    var mesh2 = new THREE.Mesh( geometry2, debugMat );
+
+    // sphere positions
+    mesh1.position.set(  1, 1, 1 );
+    mesh2.position.set( -1,-1,-1 );
+
+    // box positions
+    // mesh1.position.set(-0.5,-0.5,-0.5);
+    // mesh2.position.set( 0.5, 0.5, 0.5);
+
+    var joinedMesh = CSGLib.combineMeshes(mesh1, mesh2);
+    scene.add( joinedMesh );
+
+    // verify csg result
+    var vertCount = joinedMesh.geometry.vertices.length;
+    var expectedVerts = 32;
+    console.log("Expected result with %s, got %s ", expectedVerts, vertCount);
+
+    //var objString = exportToObj(scene);
+    //if (!container) {
+        //console.log(objString);
+        //console.warn("logged OBJ text because no container passed to display it");
+    //} else {
+        //container.innerHTML = objString; // .split( '\n' ).join( '<br />' );
+    //}
+}
+
+function testHalfEdge( scene, iterationMax = 0) {
+    // var seedGeom = new THREE.BoxGeometry( 1, 1, 1 );
+    // var seedGeom = new THREE.PlaneGeometry( 1, 1, 1 );
+    var seedGeom = generate2HoleCube();
+    var seedMesh = new THREE.Mesh( seedGeom, material );
+    scene.add( seedMesh );
+
+
+    var iterativeGeo = new THREE.Geometry();
+    var heMesh = new HELib.HEMesh({ geometry: seedGeom });
+    heMesh.closeHoles();
+    heMesh.generateFaceArrows( scene );
+
+    var iterator = heMesh.iterateFrontier();
+
+    var waitForPrompting = false;
+    var useFaceFragments = false;
+    if (!useFaceFragments) {
+        iterativeGeo.vertices = heMesh.vertices.map(function(v) {
+            return v.position;
+        });
     }
 
-    // starting from initial source face, spread from the initial point using topological information
-    // init frontier, add 3 vertices
-    var toThreeMesh = function(mesh, scene) {
-        // frontier is the collection of half edges interior to the thus far processed surface
-        var geo = new THREE.Geometry();
+    var expansion;
+    var counter = 0;
+    var button = document.getElementById('stepButton');
+    var iterativeMesh; //  = new THREE.Mesh( iterativeGeo, iterativeMat );
+    var iterativeMeshes = [];
 
-        var debugMaterial = new THREE.MeshPhongMaterial();
-        debugMaterial.side = THREE.DoubleSide;
-
-        mesh.iterateFrontier(function(expansion){
-
-            addThreeTri(expansion, geo);
-
-            var face = expansion.faceEdge.face;
-            if (!face) {
-                return;
-            }
-
-            face.generateDebugEdges();
-            expansion.faceEdge.loopEdges(function(edge){
-                if (expansion.newEdgeIDs.indexOf(edge.id) > -1) {
-                    edge.arrow.setColor( heEdge.colors.frontier );
-                }
-                scene.add(edge.arrow);
-            });
-            //renderer.render(scene, camera);
-        });
-
-        geo.computeBoundingSphere();
-        geo.computeFaceNormals();
-        geo.computeVertexNormals();
-
-        var threeMesh = new THREE.Mesh( geo, debugMaterial );
-        debugMaterial.color.setHex( Math.random() * 0xffffff );
-        debugMaterial.needsUpdate;
-        return threeMesh;
-    };
-
-    // should be broken up into addThreeVerts and addThreeFace, and do the face
-    // check in the parent func
-    var addThreeTri = function(expansionData, geo) {
-        var numNewEdges = expansionData.newEdgeIDs.length;
-        var originalEdge = expansionData.faceEdge;
-
-        // special case for the algo initialization
-        if (numNewEdges == 3) {
-            originalEdge.loopEdges(function(edge){
-                geo.vertices[edge.vert.id] = edge.vert.position;
-            });
-        } else if (numNewEdges == 2) {
-            var newVert = originalEdge.prev.vert;
-            if (!geo.vertices[newVert.id]) {
-                geo.vertices[newVert.id] = newVert.position;
-            }
-        }
-        var face = originalEdge.face;
-        if (!face) {
+    function iterationLimitedStep() {
+        if (counter >= iterationMax && iterationMax > 0) {
             return;
         }
+        // console.log("on %s of %s", counter, iterationMax);
 
-        var vertIndices = [];
-        originalEdge.loopEdges(function(edge){
-            vertIndices.push(edge.vert.id);
-        });
-        var newFace = new THREE.Face3( vertIndices[0], vertIndices[1], vertIndices[2] );
-        geo.faces.push( newFace );
-    };
+        expansion = stepIterator(iterator, iterativeGeo, useFaceFragments);
 
-    that.fromThreeMesh = fromThreeMesh;
-    that.toThreeMesh = toThreeMesh;
-    return that;
-})();*/
+        if (waitForPrompting) {
+            iterativeMesh = new THREE.Mesh( iterativeGeo.clone(), iterativeMat );
+            iterativeMeshes[counter] = iterativeMesh;
+            scene.add( iterativeMeshes[counter] );
+
+            if (expansion.done) {
+                counter++;
+                button.removeEventListener('click', iterationLimitedStep);
+                return;
+            }
+        }
+        counter++;
+    }
+
+    iterationLimitedStep();
+    if (waitForPrompting) {
+        button.addEventListener('click', iterationLimitedStep);
+    } else {
+        while (!expansion.done) {
+            iterationLimitedStep();
+        }
+    }
+
+    if (!waitForPrompting) {
+        iterativeMesh = new THREE.Mesh( iterativeGeo, iterativeMat );
+        scene.add( iterativeMesh );
+    }
+}
+
+function stepIterator(iterator, geometry, duplicateVertices) {
+    var expansion = iterator.next();
+    // assume this means it just finished since the calling also loop monitors done
+    if (expansion.done) { 
+        console.log("terminal value: ", expansion.value);
+        return expansion;
+    }
+
+    expansion.value.newEdges.map(function(newFrontierEdge){
+        newFrontierEdge.arrow.setColor( frontierColor );
+    });
+    expansion.value.closedEdges.map(function(closedEdge){
+        if (closedEdge.twin.isBoundary) {
+            closedEdge.arrow.setColor( boundaryColor );
+        } else {
+            closedEdge.arrow.setColor( interiorColor );
+        }
+    });
+
+    if (duplicateVertices) {
+        addSeparateFaceToGeometry(expansion.value.face, geometry);
+    } else {
+        addFaceToGeometry(expansion.value.face, geometry);
+    }
+
+    return expansion;
+}
+
+function addSeparateFaceToGeometry(face, geo) {
+    var index = geo.vertices.length;
+    face.edge.loopEdges(function(edge, initial){
+        geo.vertices.push(edge.vert.position.clone());
+    });
+
+    var a = index++;
+    var b = index++;
+    var c = index++;
+    geo.faces.push( new THREE.Face3(a,b,c) );
+}
+
+function addFaceToGeometry(face, geo) {
+    var f = face.edge.loopEdges(function(edge){
+        return edge.vert.index;
+    });
+    geo.faces.push( new THREE.Face3(f[0], f[1], f[2]) );
+}
+
+function generate2HoleCube() {
+    var geom = new THREE.Geometry(); 
+
+    geom.vertices = [
+        new THREE.Vector3(1.000000, -1.000000, -1.000000),
+        new THREE.Vector3(1.000000, -1.000000, 1.000000),
+        new THREE.Vector3(-1.000000, -1.000000, 1.000000),
+        new THREE.Vector3(-1.000000, -1.000000, -1.000000),
+        new THREE.Vector3(1.000000, 1.000000, -0.999999),
+        new THREE.Vector3(0.999999, 1.000000, 1.000001),
+        new THREE.Vector3(-1.000000, 1.000000, 1.000000),
+        new THREE.Vector3(-1.000000, 1.000000, -1.000000)
+    ];
+    geom.faces = [
+        new THREE.Face3(1, 4, 5),
+        new THREE.Face3(1, 0, 4),
+        new THREE.Face3(2, 5, 6),
+        new THREE.Face3(2, 1, 5),
+        new THREE.Face3(3, 6, 7),
+        new THREE.Face3(3, 2, 6),
+        new THREE.Face3(7, 0, 3),
+        new THREE.Face3(7, 4, 0)
+    ];
+    return geom;
+}
+
+module.exports = { HEMesh, CSGLib, testHalfEdge, testCSG };
 
--- src/csg/index.js
+++ src/csg/index.js
@@ -1,5 +1,13 @@
 
+
+// currently using this lib, purportedly a wrapper around the evanw/csg.js functionality
+// however the code looks pretty different, probably forked a while ago
+// https://github.com/chandlerprall/ThreeCSG
 var ThreeBSP = require("three-bsp");
+
+// candidate for further testing, this is the model js implementation
+// https://github.com/evanw/csg.js
+//var CSG = require();
 
 function combineMeshes(mesh1, mesh2) {
     var bsp1 = new ThreeBSP( mesh1 );
